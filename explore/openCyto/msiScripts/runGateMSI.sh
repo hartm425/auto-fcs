42030707c8de0d366b3a59cb9f7a678eb15c2496
jlanej <jlanej@gmail.com>
2017-08-03 16:14:08 -0500

    Lymph/pbmc only setup

--- explore/openCyto/msiScripts/runGateMSI.sh
+++ explore/openCyto/msiScripts/runGateMSI.sh
@@ -2,12 +2,16 @@
 
 OUTDIR="/scratch.global/lanej/flow/full/results_r8/"
 
-head -n4 /home/pankrat2/shared/bin/auto-fcs/explore/openCyto/dc.dev.c.csv > $OUTDIR
+p2=$OUTDIR"p2Trim.txt"
+p1=$OUTDIR"p1Trim.txt"
 
-java -jar /home/pankrat2/lane0212/genvisisOC.jar one.JL.fcs.OpenCyto inputFCS=/scratch.global/lanej/flow/full/fcs/ panel1Map=/home/pankrat2/shared/bin/auto-fcs/explore/openCyto/panel1Map.txt panel2Map=/home/pankrat2/shared/bin/auto-fcs/explore/openCyto/panel2Map.txt templateLymph=/home/pankrat2/shared/bin/auto-fcs/explore/openCyto/lymph.dev.e.csv outDir=$OUTDIR rSource=/home/pankrat2/shared/bin/auto-fcs/explore/openCyto/Lymph_monoWithQC_v5.R templateMonocyte= mapFile=/home/pankrat2/shared/bin/auto-fcs/explore/openCyto/fcsMapBlankMap.txt genvisis=/home/pankrat2/lane0212/genvisisOC.jar batch=16 memoryInMb=30000 threads=1 wallTimeInHour=100
+head -n9 /home/pankrat2/shared/bin/auto-fcs/explore/openCyto/dc.dev.c.txt > $p2
+head -n4 /home/pankrat2/shared/bin/auto-fcs/explore/openCyto/lymph.dev.e.txt > $p1
 
+# p1=/home/pankrat2/shared/bin/auto-fcs/explore/openCyto/lymph.dev.e.txt
+# p2=/home/pankrat2/shared/bin/auto-fcs/explore/openCyto/dc.dev.c.txt
 
-#java -jar /home/pankrat2/lane0212/genvisisOC.jar one.JL.fcs.OpenCyto inputFCS=/scratch.global/lanej/flow/full/fcs/ panel1Map=/home/pankrat2/shared/bin/auto-fcs/explore/openCyto/panel1Map.txt panel2Map=/home/pankrat2/shared/bin/auto-fcs/explore/openCyto/panel2Map.txt templateLymph=/home/pankrat2/shared/bin/auto-fcs/explore/openCyto/lymph.dev.e.csv outDir=$OUTDIR rSource=/home/pankrat2/shared/bin/auto-fcs/explore/openCyto/Lymph_monoWithQC_v5.R templateMonocyte=/home/pankrat2/shared/bin/auto-fcs/explore/openCyto/dc.dev.c.csv mapFile=/home/pankrat2/shared/bin/auto-fcs/explore/openCyto/fcsMapBlankMap.txt genvisis=/home/pankrat2/lane0212/genvisisOC.jar batch=16 memoryInMb=30000 threads=1 wallTimeInHour=100
+java -jar /home/pankrat2/lane0212/genvisisOC.jar one.JL.fcs.OpenCyto inputFCS=/scratch.global/lanej/flow/full/fcs/ panel1Map=/home/pankrat2/shared/bin/auto-fcs/explore/openCyto/panel1Map.txt panel2Map=/home/pankrat2/shared/bin/auto-fcs/explore/openCyto/panel2Map.txt templateLymph=$p1 outDir=$OUTDIR rSource=/home/pankrat2/shared/bin/auto-fcs/explore/openCyto/Lymph_monoWithQC_v5.R templateMonocyte=$p2 mapFile=/home/pankrat2/shared/bin/auto-fcs/explore/openCyto/fcsMapBlankMap.txt genvisis=/home/pankrat2/lane0212/genvisisOC.jar batch=16 memoryInMb=30000 threads=1 wallTimeInHour=100
 
 cd "$OUTDIR"
-sed -i 's/nodes=1/nodes=cn5601/g' *.pbs
+#sed -i 's/nodes=1/nodes=cn5601/g' *.pbs
