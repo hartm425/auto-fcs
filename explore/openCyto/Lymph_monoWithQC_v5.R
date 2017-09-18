7785afe980dab3a05c7ddcc895d8106c0f1db5c7
jlanej <jlanej@gmail.com>
2017-09-18 17:11:19 -0500

    r15

--- explore/openCyto/Lymph_monoWithQC_v5.R
+++ explore/openCyto/Lymph_monoWithQC_v5.R
@@ -68,7 +68,7 @@ fcsFilesAll <-
 # BCELL test: 2016-05-11_PANEL 1_ZF_panel one_F1631925_005.fcs
 # fcsFilesAll[grepl("PANEL 1",fcsFilesAll)]
 # fcsFilesAll =c( "2017-05-02_PANEL 1_FORTESSA_DHS_group one_F1640599_029.fcs")
-fcsFilesAll =fcsFilesAll[grepl("PANEL 1",fcsFilesAll)]
+# fcsFilesAll =fcsFilesAll[grepl("PANEL 1",fcsFilesAll)]
 fcsFilesAllProbs = c("NONE")
 
 
@@ -79,7 +79,7 @@ fcsFilesAllProbs = c("NONE")
 
 REPLACE_FOR_NEW_FILES = ""
 
-fcsFilesAll = split(fcsFilesAll, ceiling(seq_along(fcsFilesAll) / 5))
+fcsFilesAll = split(fcsFilesAll, ceiling(seq_along(fcsFilesAll) / 15))
 
 getStats <- function(gs1, qcVersion, metric, gate) {
   autoStats = getPopStats(gs1, statistic = metric)
