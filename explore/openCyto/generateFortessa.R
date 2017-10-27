73b31fbe238614068e231fe331633f0da6dc2d3d
jlanej <jlanej@gmail.com>
2017-10-27 10:46:25 -0500

    :floppy_disk:

--- explore/openCyto/generateFortessa.R
+++ explore/openCyto/generateFortessa.R
@@ -2,30 +2,31 @@
 
 
 
+
 convertP1ToFortessa <- function(templateFile, outputDir) {
   outFile = paste0(outputDir, basename(gsub("LSR", "FORTESSA", templateFile)))
   template = read.delim(templateFile, stringsAsFactors = FALSE)
-  # template[which(template$alias == "lymph"), c("gating_args")] =
-  template[which(template$pop == "CD3+/-"), c("gating_args")] ="gate_range=c(140,185)"
-  template[which(template$pop == "CD19+/-"), c("gating_args")] ="gate_range=c(100,155),adjust=1.5"
-  
-    write.table(
-      x = template,
-      file = outFile,
-      row.names = FALSE,
-      quote = FALSE,
-      sep = "\t"
-    )
+  template = converTCellGate(template = template)
+  write.table(
+    x = template,
+    file = outFile,
+    row.names = FALSE,
+    quote = FALSE,
+    sep = "\t"
+  )
   return(outFile)
 }
 
+converTCellGate  <- function(template) {
+  template[which(template$pop == "CD3+/-"), c("gating_args")] = "gate_range=c(140,185)"
+  template[which(template$pop == "CD19+/-"), c("gating_args")] = "gate_range=c(100,155),adjust=1.5"
+  return(template)
+}
 convertP2ToFortessa <- function(templateFile, outputDir) {
   outFile = paste0(outputDir, basename(gsub("LSR", "FORTESSA", templateFile)))
   template = read.delim(templateFile, stringsAsFactors = FALSE)
-  # template[which(template$alias == "lymph"), c("gating_args")] =
-  template[which(template$pop == "CD3+/-"), c("gating_args")] ="gate_range=c(140,185)"
-  template[which(template$pop == "CD19+/-"), c("gating_args")] ="gate_range=c(100,155),adjust=1.5"
   
+  template = converTCellGate(template = template)
   write.table(
     x = template,
     file = outFile,
