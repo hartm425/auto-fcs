319e727e343674335c10851b01246470f656e9d8
jlanej <jlanej@gmail.com>
2017-11-28 12:00:16 -0600

    update to Fortessa Bcell ptailgate, closes #67

--- explore/openCyto/msiScripts/syncViz.sh
+++ explore/openCyto/msiScripts/syncViz.sh
@@ -6,9 +6,9 @@ baseInDir="/scratch.global/lanej/flow/full/results_"$rev"/FULL/"
 outDir=/home/thyagara/shared/HRS/annotation/$rev
 mkdir -p $outDir"panel1/"
 mkdir -p $outDir"panel2/"
+rsync -rv $baseInDir*/panel1Vis/* $outDir"panel1/"
 
 rsync -rv $baseInDir*/panel2Vis/* $outDir"panel2/"
 
-rsync -rv $baseInDir*/panel1Vis/* $outDir"panel1/"
 
 
